990388fb163be05d0d372227fbab965ba4bdfb61
ivan.andreski <ivan.andreski@gmail.com>
2022-09-18 14:37:09 +0200

    react api url change

--- client/src/custom-axios/axios.js
+++ client/src/custom-axios/axios.js
@@ -5,7 +5,7 @@ const { REACT_APP_BACKEND_URL } = process.env;
 const instance = axios.create({
   // baseURL: 'http://localhost:5000',
   // baseURL: 'http://wp.finki.ukim.mk/fbw/',
-  baseURL: REACT_APP_BACKEND_URL,
+  baseURL: REACT_APP_BACKEND_URL + "/api",
   headers: {
     "Access-Control-Allow-Origin": "*",
   },

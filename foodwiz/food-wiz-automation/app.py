5dbada78797df7e3714264a3ad9ce246d48ddfae
ivan.andreski <ivan.andreski@gmail.com>
2022-11-27 16:18:32 +0100

    docker working

--- foodwiz/food-wiz-automation/app.py
+++ foodwiz/food-wiz-automation/app.py
@@ -15,8 +15,14 @@ nerTagRepository = DefaultNerTagRepository(tokenizerRepository)
 @app.route('/api/convert-annotation-spans', methods=['POST'])
 def convert_spans():
     json_data = request.json
-    annotationSpanDatasetTags = [AnnotationSpanDatasetTag(i) for i in json_data['annotationSpansDatasetTags']]
+    annotationSpanDatasetTags = [AnnotationSpanDatasetTag(
+        i) for i in json_data['annotationSpansDatasetTags']]
     tokens = nerTagRepository.annotation_spans_to_ner_array(
         json_data["text"], annotationSpanDatasetTags)
 
     return jsonify(Utilities.serialize_list(tokens))
+
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"text": "Hello"})

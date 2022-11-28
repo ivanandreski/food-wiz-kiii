c3b0d3ea27f5218f529c6460e917dd5e427fa5d6
ivan.andreski <ivan.andreski@gmail.com>
2022-11-28 08:01:29 +0100

    redeploy.sh change

--- foodwiz/redeploy.sh
+++ foodwiz/redeploy.sh
@@ -2,16 +2,12 @@
 
 #docker ps | grep food-base-web | awk '{print $1}' | xargs docker stop
 
-# docker-compose down
+docker-compose down
 # docker image rm -f food-wiz-backend food-wiz-client food-wiz-automation
 
 git pull
 
-cd ./food-wiz-backend
-mvn clean compile assembly:single
-cd ..
-
-# docker-compose  up -d
+docker-compose  up -d
 
 #docker build -t food-base-web:latest .
 #docker run -d -p 5000:5000 food-base-web
